29975c2a4becdcdd8f58a3f34c62a92ff1ac619c
Marais Lee <maraisienne.lee@gmail.com>
2024-01-09 22:23:42 +0900

    feat: 폴더 구조 생성

---
+++ src/app/(afterLogin)/home/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+	<div>pages</div>
+  )
+}
+
+export default HomePage
---
+++ src/app/(afterLogin)/layout.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function AfterLoginLayot ({children})  {
+	return <div>{children}AfterLoginLayout</div>;
+};
+
